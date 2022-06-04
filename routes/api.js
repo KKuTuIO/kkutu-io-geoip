8b99305a3c81a0b7117ba4bbcdfff6f35e1f1743
Do Hoon Kim <op@kdh.io>
2022-06-05 03:27:31 +0900

    Update

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,34 @@
+const geoip = require('geoip-country')
 const express = require('express')
 const router = express.Router()
-const maxmind = require('maxmind')
 const config = require('../config.json')
+const invalidMsg = {
+    "status": 400,
+    "message": "Invalid",
+    "geoLocation": {
+        "country": "INVALID",
+        "country_registered": "INVALID",
+        "continent": "INVALID"
+    }
+}
 
-maxmind.open(config.MAXMIND_MMDB_PATH).then((lookup) => {
-    router.get('/lookup', (req, res, next) => {
-        if(!req) return res.error(400)
-        if(!req.headers || !req.params) return res.error(400)
-        if(req.params.key != config.API_KEY) return res.error(400)
-        if(!req.headers["True-Client-IP"]) return res.error(400)
+router.get('/lookup', (req, res, next) => {
+    if(!req) return res.error(400)
+    if(!req.headers || !req.params) return res.json(invalidMsg)
+    if(req.params.key != config.API_KEY) return res.json(invalidMsg)
+    if(!req.headers["True-Client-IP"]) return res.json(invalidMsg)
 
-        const geoLocation = lookup.get(req.headers["True-Client-IP"])
-        res.json({
-            "status": 200,
-            "message": "OK",
-            "geoLocation": {
-                "country": geoLocation.country.iso_code,
-                "country_registered": geoLocation.registered_country.iso_code,
-                "continent": geoLocation.country.continent
-            }
-        })
-        console.log(`GET /lookup from ${req.headers["True-Client-IP"]}, ${geoLocation.country.iso_code}`)
+    const geoLocation = geoip.get(req.headers["True-Client-IP"])
+    res.json({
+        "status": 200,
+        "message": "OK",
+        "geoLocation": {
+            "country": geoLocation.country.iso_code,
+            "country_registered": geoLocation.registered_country.iso_code,
+            "continent": geoLocation.country.continent
+        }
     })
+    console.log(`GET /lookup from ${req.headers["True-Client-IP"]}, ${geoLocation.country.iso_code}`)
 })
 
 module.exports = router
