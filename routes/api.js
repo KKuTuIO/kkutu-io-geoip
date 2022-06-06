018305a4bb86b7b9183ab3fc01547b5a4caf8d40
Do Hoon Kim <op@kdh.io>
2022-06-06 16:41:01 +0900

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -12,23 +12,28 @@ const invalidMsg = {
     }
 }
 
-router.get('/lookup', (req, res, next) => {
-    if(!req) return res.error(400)
-    if(!req.params) return res.json(invalidMsg)
-    if(req.params.key != config.API_KEY) return res.json(invalidMsg)
-    if(!req.params.ip) return res.json(invalidMsg)
+router.get('/lookup/:ip/:key', (req, res, next) => {
+    try {
+        if(!req) return res.error(400)
+        if(!req.params) return res.json(invalidMsg)
+        if(req.params.key != config.API_KEY) return res.json(invalidMsg)
+        if(!req.params.ip) return res.json(invalidMsg)
 
-    const geoLocation = geoip.lookup(req.params.ip)
-    res.json({
-        "status": 200,
-        "message": "OK",
-        "geoLocation": {
-            "country": geoLocation.country.iso_code,
-            "country_registered": geoLocation.registered_country.iso_code,
-            "continent": geoLocation.country.continent
-        }
-    })
-    console.log(`GET /lookup from ${req.params.ip}, ${geoLocation.country.iso_code}`)
+        const geoLocation = geoip.lookup(req.params.ip)
+        res.json({
+            "status": 200,
+            "message": "OK",
+            "geoLocation": {
+                "country": geoLocation.country.iso_code,
+                "country_registered": geoLocation.registered_country.iso_code,
+                "continent": geoLocation.country.continent
+            }
+        })
+        console.log(`GET /lookup from ${req.params.ip}, ${geoLocation.country.iso_code}`)
+
+    } catch (e) {
+        console.error(`[ERROR] ${e}`)
+    }
 })
 
 module.exports = router
